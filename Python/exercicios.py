35a20250cd01fea01c8c4ab697d1dc84a35459fc
Carlos Eduardo <carloseduardodudu558@hotmail.com>
2026-02-10 21:01:21 -0300

    aula - 10-02

---
+++ Python/exercicios.py
@@ -0,0 +1,49 @@
+# ----- 1.1 - variaveis
+
+# peso = float(input("Peso: "))
+# altura = float(input("altura: "))
+
+# saida = peso/(altura**2)
+
+# print("IMC: ",saida)
+
+# ----- 2.10 if
+
+# usuario = input("Usuario: ")
+# senha = input("Senha: ")
+
+# if usuario=="admin" and senha=="1234":
+#     print("login bem-sucedido")
+# else:
+#     print("login falhou")
+
+# ----- for
+# exemplo 1
+# for i in range(1, 6):
+#     print("Número:", i)
+
+# exemplo 2
+
+# for i in range(1, 11):
+#     resultado = 5 * i
+#     print(f"5 x {i} = {resultado}")
+
+# exemplo 3
+
+# resposta = 0
+# numero = int(input("Número: "))
+
+# for i in range(1, numero):
+#     resposta = resposta + i
+#     print(f"Resultado: {resposta-i} + {i} = {resposta}")
+
+# # piramide 
+
+num = int(input("Numero: "))
+for i in range(1,num):
+    print("O"*i)
+
+# ----- exercicio 3 - FOR
+
+
+
