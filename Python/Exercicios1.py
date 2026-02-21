4692d3abfdedf207a2ba4ee2305af426ca9017f7
Carlos Eduardo <carloseduardodudu558@hotmail.com>
2026-02-20 21:01:36 -0300

    Em casa - 20/02

---
+++ Python/Exercicios1.py
@@ -0,0 +1,88 @@
+# ----- 1.1 - variaveis
+
+# peso = float(input("Peso: "))
+# altura = float(input("altura: "))
+
+# saida = peso/(altura**2)
+
+# print("IMC: ",saida)
+
+# ----- 1.2
+
+# temperatura = float(input("Temp em Celcius: "))
+
+# F = temperatura*1.8+32
+
+# print("Conversao: ", F)
+
+# ----- 1.3
+
+# preco = float(input("Preço: "))
+# porcentagem = float(input("Porcentagem: "))
+
+# saida = preco-(preco*porcentagem/100)
+
+# print("Preço final: ",saida)
+
+# ----- 1.4
+
+# idade = int(input("Idade: "))
+
+# saida = idade*365
+
+# print("Resultado: ", saida)
+
+# ----- 1.5
+
+# reais = float(input("Valor em Reais: R$ "))
+# dolar = float(input("Cotação do Dolar: $ "))
+
+# saida = dolar*reais
+
+# print("Valor total: $ ", saida)
+
+# ----- 1.6
+
+# distancia = int(input("Distância: KM "))
+# consumo = float(input("Comsumo do carro: "))
+
+# saida = distancia/consumo
+
+# print("Resultado: ", saida)
+
+# ----- 1.7
+
+# nota1 = float(input("Nota 1: "))
+# nota2 = float(input("Nota 2: "))
+# nota3 = float(input("Nota 3: "))
+
+# saida = (nota1+nota2+nota3)/3
+
+# print("Medias das notas: ", saida)
+
+# ----- 1.8
+
+# largura = int(input("Largura: m "))
+# comprimento = int(input("Comprimento: m "))
+
+# saida = largura*comprimento
+
+# print("Resultado: ",saida,"m²")
+
+# ----- 1.9
+
+# salario = float(input("Salario bruto: R$ "))
+# inss = 11/100
+
+# saida = salario-(salario*inss)
+
+# print("Salario Liquido: R$ ", saida)
+
+# ----- 1.10
+
+valorCompra = float(input("Valor da Compra: R$ "))
+valorPago = float(input("Valor pago: R$ "))
+
+saida = valorCompra-valorPago
+
+print("Valor do troco: R$ ",saida)
---
+++ Python/Exercicios2.py
@@ -0,0 +1,105 @@
+# 2.1 ------
+
+# numero = int(input("Numero inteiro: "))
+# if numero/2:
+#     print("Numero Par")
+# else:
+#     print("Numero Ìmpar")
+
+# 2.2
+
+# nota = int(input("Nota: "))
+# if nota>=7:
+#     print("Aprovado")
+# elif nota>=5 and nota<7:
+#     print("Recuperação")
+# else:
+#     print("Reprovado")
+
+# 2.3
+
+# valorCompra = float(input("Valor da Compra: R$ "))
+# if valorCompra >100:
+#     print(valorCompra-((valorCompra*10)/100))
+# else:
+#     print(valorCompra)
+
+# 2.4
+
+# idade = int(input("Idade: "))
+# if idade<=12:
+#     print("Criança")
+# elif idade>=13 and idade<=17:
+#     print("Adolescente")
+# elif idade>=18 and idade<=59:
+#     print("Adulto")
+# else:
+#     print("idoso")
+
+# 2.5
+
+# senha = input("Senha: ")
+# if senha=="python123":
+#     print("Acesso Permitido")
+# else:
+#     print("Senha incorreta")
+
+# 2.6
+
+# velocidade = int(input("Velocidade do carro: "))
+# if velocidade<=60:
+#     print("sem multa")
+# else:
+#     multa = 5*velocidade-300
+#     print("Multado em: R$ ",multa)
+
+# 2.7
+
+# numero1 = int(input("Numero 1: "))
+# numero2 = int(input("Numero 2: "))
+
+# if numero1 > numero2:
+#     print(numero1, "é Maior")
+# elif numero2 > numero1:
+#     print(numero2, "é Maior")
+# else:
+#     print("São iguais")
+
+# 2.8
+
+# lado1 = int(input("Lado 1: "))
+# lado2 = int(input("Lado 2: "))
+# lado3 = int(input("Lado 3: "))
+
+# if lado1<(lado2+lado3) and lado2<(lado1+lado3) and lado3<(lado1+lado2):
+#     print("Forma um Triangulo")
+# else:
+#     print("Não forma")
+
+# 2.9
+
+# peso = float(input("Peso: "))
+# altura = float(input("altura: "))
+
+# IMC = peso/(altura**2)
+
+# if IMC<18.5:
+#     print("Abaixo do peso")
+# elif IMC>=18.5 and IMC<=24.9:
+#     print("Peso normal")
+# elif IMC>=25 and IMC<=29.9:
+#     print("Sobrepeso")
+# else:
+#     print("Obesidade")
+
+# 2.10
+
+usuario = input("Usuario: ")
+senha = input("Senha: ")
+
+if usuario=="admin" and senha=="1234":
+    print("login bem-sucedido")
+else:
+    print("login falhou")
+
+    
