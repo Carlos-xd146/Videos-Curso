fe52deacdf26495cd41eec82104a29b1e96d3efa
Carlos Eduardo <carloseduardodudu558@hotmail.com>
2026-02-22 08:44:47 -0300

    em casa - 22/02

---
+++ Python/Exercicios3.py
@@ -0,0 +1,42 @@
+# 3.1
+
+# num = 10
+
+# for i in range(0, 10):
+#     resposta = num - i
+    
+#     print(f"{resposta}")
+# print(f"FIM!")
+
+
+# 3.2
+
+# numero = int(input("Número: "))
+
+# for i in range(1, numero):
+#     resposta = numero * i
+#     print(f"Resultado: {numero}*{i} = {resposta}")
+
+# 3.3
+
+# resposta = 0 
+# num = int(input("Numero: "))
+
+# for i in range(1, num+1):
+#     resposta = resposta + i
+# print(resposta)
+
+# 3.4
+
+# for i in range (1, 50):
+#     if (i % 2)-1:
+#         print(i) 
+
+# 3.5
+
+# for i in range (1, 6):
+#     print(input(f"Numero{i}: "))
+
+
+# 3.6
+
