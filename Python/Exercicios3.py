3afdc1628c26108aaa93bdd13f31591495d5097b
Carlos Eduardo <carloseduardodudu558@hotmail.com>
2026-02-24 21:03:29 -0300

    aula - 24/02

--- Python/Exercicios3.py
+++ Python/Exercicios3.py
@@ -28,15 +28,65 @@
 
 # 3.4
 
-# for i in range (1, 50):
+# for i in range (1, 52):
 #     if (i % 2)-1:
 #         print(i) 
 
 # 3.5
+total_par = 0
+total_impar = 0
+total_positivo = 0
+total_negativo = 0
 
-# for i in range (1, 6):
-#     print(input(f"Numero{i}: "))
 
+for i in range (1, 6):
+    numero_x = int(input(f"Numero {i}: "))
+    if numero_x <0:
+        total_negativo = total_negativo+1
+    if numero_x >0:
+        total_positivo = total_positivo+1
+    if numero_x % 2 == 0:
+        total_par = total_par+1
+    else:
+        total_impar = total_impar+1
+    
+    primo = True
+    for i in range(2, numero_x):
+        print(numero_x,"%", i,"=",numero_x%i)
+        if numero_x%i == 0:
+            primo = False
+            break
+    
+    
+
+        
+print("Existe", total_negativo, "números negativos.")    
+print("Existe", total_positivo, "números positivos.")
+print("Existe", total_par, "números pares.")
+print("Existe", total_impar, "números inpares.")
 
 # 3.6
+
+# for i in range (1, 6):
+#     input(f"Nota {i}: ")
+
+
+
+
+
+
+# 3.10
+
+# numero = int(input("Insira o numero: "))
+# primo = True
+# for i in range(2, numero):
+#     print(numero,"%", i,"=",numero%i)
+#     if numero%i == 0:
+#         primo = False
+#         break
+
+# if primo: 
+#     print(numero,"é Primo")
+# else:
+#     print(numero,"não é primo")
 
---
+++ Python/exercicios4.py
@@ -0,0 +1,27 @@
+# exemplo 1: função
+
+# def saudar(nome):
+#     mensagem = f"Olá, {nome}!"
+#     return mensagem
+
+# resultado = saudar("Ana")
+# print(resultado)
+
+# Exemplo 2 : calculadora simples
+
+# def somar(a, b):
+#     return a + b
+
+# resultado = somar(10, 5)
+# print("Resultado: ", resultado)
+
+# desafio:
+
+num1 = print(input("Numero1: "))
+num2 = print(input("Numero2: "))
+
+def div (num1,num2): 
+    return num1 + num2
+
+resultado = div()
+print(resultado)    
